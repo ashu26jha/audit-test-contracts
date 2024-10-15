ba97092ff7ac3482dca3e738718658b21d4150df
Bilgin <kocakbilgin@gmail.com>
2024-10-15 12:38:43 +0300

    feat: telegram handle added

--- frontend/config/constants.ts
+++ frontend/config/constants.ts
@@ -2,7 +2,7 @@ export const AA_EMAIL = "auditagent@nethermind.io";
 
 export const CONTACT_PAGE_URL = "https://auditagent.nethermind.io/contact-us";
 export const DISCLAIMER_PAGE_URL = "https://auditagent.nethermind.io/disclaimer";
-export const TELEGRAM_URL = "#";
+export const TELEGRAM_URL = "https://t.me/kirill_balakhonov";
 
 export const MAX_TOKENS = 40000;
 // Modify this in production
