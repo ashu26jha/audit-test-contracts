ea8ac177aa03accffc8e579fcdfb28c5a1330322
Pedrojok01 <77141890+Pedrojok01@users.noreply.github.com>
2024-10-16 14:09:45 +0700

    Remove CC email to Kirill (#93)
    * Remove CC email to Kirill
    
    * Edit terms of use links
    
    * Edit footer links size & color on login page

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -80,7 +80,6 @@ SMTP_SERVER = os.getenv("SMTP_SERVER")
 SMTP_PORT = os.getenv("SMTP_PORT", 587)
 SMTP_USERNAME = os.getenv("SMTP_USERNAME")
 SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
-CC_EMAIL = os.getenv("CC_EMAIL")
 
 ##################################################
 #                   REDIS
--- frontend/config/constants.ts
+++ frontend/config/constants.ts
@@ -1,7 +1,7 @@
 export const AA_EMAIL = "auditagent@nethermind.io";
 
 export const CONTACT_PAGE_URL = "https://auditagent.nethermind.io/contact-us";
-export const DISCLAIMER_PAGE_URL = "https://auditagent.nethermind.io/disclaimer";
+export const DISCLAIMER_PAGE_URL = "https://auditagent.nethermind.io/terms-of-use";
 export const TELEGRAM_URL = "https://t.me/kirill_balakhonov";
 
 export const MAX_TOKENS = 40000;
