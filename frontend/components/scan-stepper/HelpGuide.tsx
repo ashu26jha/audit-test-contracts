ba52f61c31b2a4e99b1e36e9aa85dab12f5015e8
Bilgin Koçak <kocakbilgin@gmail.com>
2024-10-29 13:33:14 +0300

    fix:lint issue (#120)

--- frontend/components/scan-stepper/HelpGuide.tsx
+++ frontend/components/scan-stepper/HelpGuide.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
+
 import { Progress, Divider } from "@nextui-org/react";
 import Image from "next/image";
+
 import { useScanStepperStore } from "../../store/scanStepperStore";
 
 export const HelpGuide: React.FC<{
