5c6bf9f90a5cd70e1942375c1bd2a5e89b5838d4
Bilgin Koçak <kocakbilgin@gmail.com>
2024-10-29 12:55:31 +0300

    fix: import order (#119)

--- frontend/components/scan-stepper/HelpGuide.tsx
+++ frontend/components/scan-stepper/HelpGuide.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { Progress, Divider } from "@nextui-org/react";
-import { useScanStepperStore } from "../../store/scanStepperStore";
 import Image from "next/image";
+import { useScanStepperStore } from "../../store/scanStepperStore";
 
 export const HelpGuide: React.FC<{
   selectedLines: number;
