008277e9f919fde5609790fa780c75e14db2bff4
Pedrojok01 <77141890+Pedrojok01@users.noreply.github.com>
2024-11-22 13:26:59 +0700

    Remove health check throttle (#204)

--- backend/api/v1/endpoints/health_check.py
+++ backend/api/v1/endpoints/health_check.py
@@ -1,12 +1,10 @@
 from fastapi import APIRouter
 
 from api.v1.schemas.api_response_schema import SuccessResponse
-from common.throttling import throttle
 
 router = APIRouter()
 
 
 @router.get("/health-check", response_model=SuccessResponse)
-@throttle(rate_limit_minutes=1 / 60)
 async def health_check():
     return SuccessResponse(data={"details": "All systems operational"})
